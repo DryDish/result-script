72188dca2ac20a33faab520c7c325ffed15871f9
DryDish <57399961+DryDish@users.noreply.github.com>
2023-08-29 20:06:50 +0200

    add more tests

--- tests/performance.asyncMethods.test.ts
+++ tests/performance.asyncMethods.test.ts
@@ -1,35 +1,79 @@
+import { Ok } from "../src/result";
 import { OkAsync } from "../src/resultAsync";
 
 const numberOfTests = 10_000;
 
 describe("Testing execution time", () => {
-	test("Testing AsyncResult", async () => {
-		const startTime = performance.now();
+	describe("Testing AsyncResult", () => {
+		test(`Measure execution time of OkAsync resolving ${numberOfTests} times`, async () => {
+			const startTime = performance.now();
 
-		for (let i = 0; i < numberOfTests; i++) {
-			await OkAsync(123);
-		}
+			for (let i = 0; i < numberOfTests; i++) {
+				await OkAsync(123);
+			}
 
-		const endTime = performance.now();
-		const executionTime = endTime - startTime;
+			const endTime = performance.now();
+			const executionTime = endTime - startTime;
 
-		console.log(`Total execution time for ${numberOfTests} executions: ${executionTime} ms`);
+			console.log(`OkAsync: Total execution time for ${numberOfTests} executions: ${executionTime} ms`);
 
-		expect(executionTime).toBeLessThan(30);
+			expect(executionTime).toBeLessThan(70);
+		});
+
+		test(`Measure execution time of chained OkAsync resolving ${numberOfTests} times`, async () => {
+			const startTime = performance.now();
+
+			let result = Ok(111);
+			for (let i = 0; i < numberOfTests; i++) {
+				result = await OkAsync(123).andThen((x) => OkAsync(x + 1));
+			}
+
+			const endTime = performance.now();
+
+			const executionTime = endTime - startTime;
+
+			console.log(
+				`OkAsync(Promise):Total execution time for ${numberOfTests} executions: ${executionTime} ms.\n Result: `,
+				result.unwrap()
+			);
+
+			expect(executionTime).toBeLessThan(100);
+		});
 	});
+	describe("Testing Promise", () => {
+		test(`Measure execution time of Promise resolving ${numberOfTests} times`, async () => {
+			const startTime = performance.now();
 
-	test(`Measure execution time of Promise resolving ${numberOfTests} times`, async () => {
-		const startTime = performance.now();
+			for (let i = 0; i < numberOfTests; i++) {
+				await Promise.resolve(123);
+			}
 
-		for (let i = 0; i < numberOfTests; i++) {
-			await Promise.resolve(123);
-		}
+			const endTime = performance.now();
+			const executionTime = endTime - startTime;
 
-		const endTime = performance.now();
-		const executionTime = endTime - startTime;
+			console.log(`Promise: Total execution time for ${numberOfTests} executions: ${executionTime} ms`);
 
-		console.log(`Total execution time for ${numberOfTests} executions: ${executionTime} ms`);
+			expect(executionTime).toBeLessThan(15);
+		});
 
-		expect(executionTime).toBeLessThan(10);
+		test(`Measure execution time of a chained promise resolving ${numberOfTests} times `, async () => {
+			const startTime = performance.now();
+
+			let result = {};
+			for (let i = 0; i < numberOfTests; i++) {
+				result = await Promise.resolve(123).then((x) => Promise.resolve(x + 1));
+			}
+
+			const endTime = performance.now();
+
+			const executionTime = endTime - startTime;
+
+			console.log(
+				`Promise(Promise):Total execution time for ${numberOfTests} executions: ${executionTime} ms.\nResult: `,
+				result
+			);
+
+			expect(executionTime).toBeLessThan(70);
+		});
 	});
 });
